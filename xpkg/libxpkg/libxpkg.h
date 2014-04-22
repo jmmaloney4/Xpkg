7d8f013a4be5071777a432a9766bb8b2196d15f7
Jack Maloney <jmmaloney4@gmail.com>
2014-04-22 17:36:47 -0500

    updates

---
+++ xpkg/libxpkg/libxpkg.h
@@ -0,0 +1,13 @@
+//
+//  libxpkg.h
+//  libxpkg
+//
+//  Created by Jack Maloney on 4/22/14.
+//  Copyright (c) 2014 IV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libxpkg : NSObject
+
+@end
---
+++ xpkg/libxpkg/libxpkg.m
@@ -0,0 +1,13 @@
+//
+//  libxpkg.m
+//  libxpkg
+//
+//  Created by Jack Maloney on 4/22/14.
+//  Copyright (c) 2014 IV. All rights reserved.
+//
+
+#import "libxpkg.h"
+
+@implementation libxpkg
+
+@end
